5ad7926059ee099710eb65fb2bf7bffab9417d21
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-25 12:57:06 +0300

    Use WeakMap

--- js/page.js
+++ js/page.js
@@ -13,6 +13,20 @@ var yearlyTotal = 0;
 
 const monthlySales = new Map();
 
+{
+  let salesA = {
+    a: [1, 2],
+  };
+
+  var map = new WeakMap();
+  map.set(salesA, "Hiking");
+
+  console.log("First " + salesA);
+}
+
+//console.log(map);
+console.log("Second " + salesA);
+
 // Add Sales
 function addSale() {
   monthlySales.set(newMonth.value, parseInt(newAmount.value));
